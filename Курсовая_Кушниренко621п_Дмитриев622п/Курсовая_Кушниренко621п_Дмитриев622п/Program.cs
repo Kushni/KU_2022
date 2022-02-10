2c9c5efc7d26a8df1595d85dd57239b01863f051
Kushni <prosto.stappro@gmail.com>
2022-02-10 08:50:17 +0200

    Небольшое изменение в FinderWay. Обнаружены проблемы со сравнением местности.

--- Курсовая_Кушниренко621п_Дмитриев622п/Курсовая_Кушниренко621п_Дмитриев622п/Program.cs
+++ Курсовая_Кушниренко621п_Дмитриев622п/Курсовая_Кушниренко621п_Дмитриев622п/Program.cs
@@ -9,8 +9,9 @@ namespace WalkingHomunculus
             Jeep jeep = new Jeep("Plyalya", "Green", new Coordinates(1, 1));
             Map.CreateMap(1);
             Console.WriteLine(jeep.ToString());
+            Map.AllCells[2].TypeLandscape = false;
             jeep.NewSpeed(1000);
-            jeep.GetDestination(new Coordinates(300, 300));
+            jeep.GetDestination(new Coordinates(100, 1));
             jeep.Move();
             Console.WriteLine(jeep.ToString());
         }
